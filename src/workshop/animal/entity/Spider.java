33c32ac65a3971f76aa22d7174dab07aef116be8
siyeon04 <seanna0417@gmail.com>
2026-03-04 15:20:20 +0900

    JavaSE Project second commit

---
+++ src/workshop/animal/entity/Spider.java
@@ -0,0 +1,13 @@
+package workshop.animal.entity;
+
+public class Spider extends Animal{
+	public Spider() {
+//		super(8);
+		super(8);
+	}
+	
+	@Override
+	public void eat() {
+		System.out.println("Spider´Â ÀÛÀº ¹ú·¹¸¦ ¸Ô¾î¿ä!!");		
+	}
+}
